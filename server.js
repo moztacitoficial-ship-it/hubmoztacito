74beb746e0d2e1aba690f3adc9748bf7cbeb46cb
Geeko18 <internauta509@gmail.com>
2026-06-26 14:16:30 -0500

    feat: implement base layout and global styles with header, sidebar, and footer components

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // Para que React Router funcione, siempre devolver index.html
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
